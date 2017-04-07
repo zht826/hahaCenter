bac3bd380139a55ebc60fead1edb80079514af81
zhanghaitao <zhanghaitao@imobpay.com>
2017-04-07 17:12:04 +0800

    1、提取接口请求函数，封装 2、刷新按钮优化

--- pages/laugh/laugh.js
+++ pages/laugh/laugh.js
@@ -1,14 +1,97 @@
 var app = getApp();
 var that;
 let n=1;
+function reqText(){
+    if(!that.data.isReq){
+        wx.showLoading({
+            title: '加载中',
+            mask:true,
+        });
+        that.setData({
+            isReq:true
+        });
+        wx.request({
+            url: that.data.apiIp, //仅为示例，并非真实的接口地址
+            data: {
+                key: that.data.key,
+                // type:"pic"
+            },
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function(res) {
+                console.log(res.data)
+                var data = res.data.result;
+                var temData = [];
+                for(var i=0;i<data.length;i++){
+                    temData[i]={};
+                    temData[i].content = data[i].content;
+                    temData[i].time = data[i].unixtime;
+                    temData[i].iconUrl = data[i].url;
+                }
+                that.setData({
+                    textModuleData: temData,
+                    isReq:false
+                });
+                wx.hideLoading();
+            }
+        })
+    }
+    
+}
+function reqPic(){
+    if(!that.data.isReq){
+        wx.showLoading({
+            title: '加载中',
+            mask:true,
+        });
+        that.setData({
+            isReq:true
+        });
+        wx.request({
+            url: that.data.apiIp, //仅为示例，并非真实的接口地址
+            data: {
+                key: that.data.key,
+                type:"pic"
+            },
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function(res) {
+                console.log(res.data)
+                var data = res.data.result;
+                var temData = [];
+                for(var i=0;i<data.length;i++){
+                    temData[i]={};
+                    temData[i].content = data[i].content;
+                    temData[i].time = data[i].unixtime;
+                    temData[i].iconUrl = data[i].url;
+                }
+                that.setData({
+                    picModuleData: temData,
+                    isReq:false
+                });
+                wx.hideLoading();
+            }
+        })
+    }
+}
+function barAnimate(n){
+    var animation = wx.createAnimation({
+        duration: 750
+    });
+    that.animation = animation;
+    animation.left(n).step();
+    that.setData({
+        barAnimationData:animation.export()
+    });
+}
 Page({
     data:{
-        userInfo:"",
-        nickName:"",
-        userInfo:"",
         isReq:false,
         rtDeg:'0',
         scrollTop:"100",
+        barLoc:'0',
         animationData: {},
         textModuleData:[
             {
@@ -35,50 +118,16 @@ Page({
         activeId:'0',
         slideIndex:'0',
         // apiIp:'http://v.juhe.cn/joke/randJoke.php',
-        apiIp:'http://koa.ngrok.zht88.top/api/Joke',
+        // apiIp:'http://koa.ngrok.zht88.top/api/Joke',
+        apiIp:'https://xcx.zht87.com/api/Joke',
         key:'8560ad007b5fb4ce2d18f6d912e0a17e'
     },
     onLoad:function(options){
         that = this;
-        
-            //调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo){
-            //更新数据
-            that.setData({
-                userInfo:userInfo
-            })
-        })
-        this.setData({
-            nickName:that.data.userInfo.nickName
-        })
-        wx.request({
-            url: that.data.apiIp, //仅为示例，并非真实的接口地址
-            data: {
-                key: that.data.key,
-                // type:"pic"
-            },
-            header: {
-                'content-type': 'application/json'
-            },
-            success: function(res) {
-                console.log(res.data)
-                var data = res.data.result;
-                var temData = [];
-                for(var i=0;i<data.length;i++){
-                    temData[i]={};
-                    temData[i].content = data[i].content;
-                    temData[i].time = data[i].unixtime;
-                    temData[i].iconUrl = data[i].url;
-                }
-                that.setData({
-                    textModuleData: temData
-                })
-            }
-        })
+        reqText();
     },
     onShow:function(){
         // 生命周期函数--监听页面显示
-
     },
     imgSuc:function(e){
         var that = this;
@@ -91,127 +140,38 @@ Page({
     },
     changeActive: function(e) {
         var that = this;
-        
         if(e.target.id==0){
-            wx.request({
-                url: that.data.apiIp, //仅为示例，并非真实的接口地址
-                data: {
-                    key: that.data.key,
-                    // type:"pic"
-                },
-                header: {
-                    'content-type': 'application/json'
-                },
-                success: function(res) {
-                    console.log(res.data)
-                    var data = res.data.result;
-                    var temData = [];
-                    for(var i=0;i<data.length;i++){
-                        temData[i]={};
-                        temData[i].content = data[i].content;
-                        temData[i].time = data[i].unixtime;
-                        temData[i].iconUrl = data[i].url;
-                    }
-                    that.setData({
-                        textModuleData: temData
-                    })
-                }
-            })
+            reqText();
+            barAnimate(0);
         }else if(e.target.id==1){
-            wx.request({
-                url: that.data.apiIp, //仅为示例，并非真实的接口地址
-                data: {
-                    key: that.data.key,
-                    type:"pic"
-                },
-                header: {
-                    'content-type': 'application/json'
-                },
-                success: function(res) {
-                    console.log(res.data)
-                    var data = res.data.result;
-                    var temData = [];
-                    for(var i=0;i<data.length;i++){
-                        temData[i]={};
-                        temData[i].content = data[i].content;
-                        temData[i].time = data[i].unixtime;
-                        temData[i].iconUrl = data[i].url;
-                    }
-                    that.setData({
-                        picModuleData: temData
-                    })
-                }
-            })
+            reqPic();
+            barAnimate('50%');
         }
         that.setData({
             activeId:e.target.id,
-            slideIndex:e.target.id
+            slideIndex:e.target.id,
+            barLoc:e.target.id
         });
     },
     changeSlide:function(e){
         var that = this;
-        if(e.detail.current==0){
-            wx.request({
-                url: that.data.apiIp, //仅为示例，并非真实的接口地址
-                data: {
-                    key: that.data.key,
-                    // type:"pic"
-                },
-                header: {
-                    'content-type': 'application/json'
-                },
-                success: function(res) {
-                    console.log(res.data)
-                    var data = res.data.result;
-                    var temData = [];
-                    for(var i=0;i<data.length;i++){
-                        temData[i]={};
-                        temData[i].content = data[i].content;
-                        temData[i].time = data[i].unixtime;
-                        temData[i].iconUrl = data[i].url;
-                    }
-                    that.setData({
-                        textModuleData: temData
-                    })
-                }
-            })
-        }else if(e.detail.current==1){
-            wx.request({
-                url: that.data.apiIp, //仅为示例，并非真实的接口地址
-                data: {
-                    key: that.data.key,
-                    type:"pic"
-                },
-                header: {
-                    'content-type': 'application/json'
-                },
-                success: function(res) {
-                    console.log(res.data)
-                    var data = res.data.result;
-                    var temData = [];
-                    for(var i=0;i<data.length;i++){
-                        temData[i]={};
-                        temData[i].content = data[i].content;
-                        temData[i].time = data[i].unixtime;
-                        temData[i].iconUrl = data[i].url;
-                    }
-                    that.setData({
-                        picModuleData: temData
-                    })
-                }
-            })
-        }
-        that.setData({
-            activeId:e.detail.current
-        });
-
+        setTimeout(function(){
+            if(e.detail.current==0){
+                reqText();
+                barAnimate(0);
+            }else if(e.detail.current==1){
+                reqPic();
+                barAnimate('50%');
+            }
+            that.setData({
+                activeId:e.detail.current,
+                barLoc:e.detail.current
+            });
+        },500);
+        
     },
     refresh:function(){
         if(!this.data.isReq){//先判断是否正在加载
-            //设置正在加载标识
-            this.setData({
-                isReq:true
-            });
             var that = this;
             let index = this.data.activeId;
             //点击后动画效果
@@ -242,57 +202,9 @@ Page({
             }
             //根据当前位置判断刷新文字页还是图片页
             if(index==0){
-                wx.request({
-                    url: that.data.apiIp, //仅为示例，并非真实的接口地址
-                    data: {
-                        key: that.data.key,
-                        // type:"pic"
-                    },
-                    header: {
-                        'content-type': 'application/json'
-                    },
-                    success: function(res) {
-                        console.log(res.data)
-                        var data = res.data.result;
-                        var temData = [];
-                        for(var i=0;i<data.length;i++){
-                            temData[i]={};
-                            temData[i].content = data[i].content;
-                            temData[i].time = data[i].unixtime;
-                            temData[i].iconUrl = data[i].url;
-                        }
-                        that.setData({
-                            textModuleData: temData,
-                            isReq:false
-                        })
-                    }
-                })
+                reqText();
             }else if(index==1){
-                wx.request({
-                    url: that.data.apiIp, //仅为示例，并非真实的接口地址
-                    data: {
-                        key: that.data.key,
-                        type:"pic"
-                    },
-                    header: {
-                        'content-type': 'application/json'
-                    },
-                    success: function(res) {
-                        console.log(res.data)
-                        var data = res.data.result;
-                        var temData = [];
-                        for(var i=0;i<data.length;i++){
-                            temData[i]={};
-                            temData[i].content = data[i].content;
-                            temData[i].time = data[i].unixtime;
-                            temData[i].iconUrl = data[i].url;
-                        }
-                        that.setData({
-                            picModuleData: temData,
-                            isReq:false
-                        })
-                    }
-                })
+                reqPic();
             }
         }
     }
